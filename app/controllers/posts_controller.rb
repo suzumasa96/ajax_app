fee11e4e971fe6dc94796155837e393c0354e8ca
suzumasa96 <masakisuzuki.shkr@gmail.com>
2020-11-16 18:14:52 +0900

    既読機能のサーバー再度実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,4 +11,22 @@ class PostsController < ApplicationController
     Post.create(content: params[:content])
     redirect_to action: :index
   end
+
+  def checked
+    #既読メモidが渡される設定、該当レコードを取得
+    post = Post.find(params[:id])
+    #既読ならば
+    if post.chcecked
+      #既読解除の為falseへ変更
+      post.update(checked: false)
+          #　↑ActiveRecordのupdateで更新
+    else
+      #既読のためtrueへ変更
+      post.update(checked: true)  
+    end
+    #更新後レコードを取得し直す
+    item = Post.find(params[:id])
+    #JSON形式でchecked.jsに返却
+    render json: { post: item }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   #投稿完了ページ不要の為
   #get 'posts/new', to: 'posts#new'
   post 'posts', to: 'posts#create'
+  get 'posts/:id', to 'posts#checked'
 end
